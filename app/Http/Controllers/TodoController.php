34ee9a58631fc153899a59f8c08b87c249fb2c82
John Carlo Yhapon <yhaponjc@gmail.com>
2024-12-08 13:27:53 +0800

    todos functions

--- app/Http/Controllers/TodoController.php
+++ app/Http/Controllers/TodoController.php
@@ -2,9 +2,37 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Todo;
 use Illuminate\Http\Request;
 
 class TodoController extends Controller
 {
-    //
+    public function index()
+    {
+        $todos = Todo::where('user_id', auth()->id())->get();
+        return view('todos.index', ['todos' => $todos]);
+    }
+
+    public function store(Request $request)
+    {
+        $request->validate(['title' => 'required|string|max:255']);
+        Todo::create([
+            'title' => $request->title,
+            'user_id' => auth()->id(),
+        ]);
+
+        return redirect()->route('todos.index');
+    }
+
+    public function update(Request $request, Todo $todo)
+    {
+        $todo->update(['completed' => $request->completed]);
+        return redirect()->route('todos.index');
+    }
+
+    public function destroy(Todo $todo)
+    {
+        $todo->delete();
+        return redirect()->route('todos.index');
+    }
 }
