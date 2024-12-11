b3621d1d8cbce730532704bc5ce8fb98f38d0a25
John Carlo Yhapon <yhaponjc@gmail.com>
2024-12-11 22:14:31 +0800

    removed unusued codes

--- routes/web.php
+++ routes/web.php
@@ -5,8 +5,12 @@ use App\Http\Controllers\AuthController;
 use App\Http\Controllers\TodoController;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('auth.login');
+})->name('login');
+
+Route::get('home', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/register', [AuthController::class, 'showRegisterForm'])->name('register.form');
 Route::post('/register', [AuthController::class, 'register'])->name('register');
@@ -17,12 +21,8 @@ Route::post('/login', [AuthController::class, 'login'])->name('login');
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
 
 Route::middleware('auth')->group(function () {
-    Route::get('/dashboard', function () {
-        return redirect()->route('todos.index');
-    })->name('dashboard');
+    Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
+    Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
+    Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
+    Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
 });
-
-Route::get('/todos', [TodoController::class, 'index'])->name('todos.index');
-Route::post('/todos', [TodoController::class, 'store'])->name('todos.store');
-Route::put('/todos/{todo}', [TodoController::class, 'update'])->name('todos.update');
-Route::delete('/todos/{todo}', [TodoController::class, 'destroy'])->name('todos.destroy');
