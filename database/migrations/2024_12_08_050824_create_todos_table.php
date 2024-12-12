6f40aa277817b673b450937c5681bd099d82c1f7
John Carlo Yhapon <yhaponjc@gmail.com>
2024-12-12 21:35:16 +0800

    added title, completed and user_id in the migration

--- database/migrations/2024_12_08_050824_create_todos_table.php
+++ database/migrations/2024_12_08_050824_create_todos_table.php
@@ -9,13 +9,16 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
-    public function up(): void
-    {
-        Schema::create('todos', function (Blueprint $table) {
-            $table->id();
-            $table->timestamps();
-        });
-    }
+    public function up()
+{
+    Schema::create('todos', function (Blueprint $table) {
+        $table->id();
+        $table->string('title');
+        $table->boolean('completed')->default(false);
+        $table->foreignId('user_id')->constrained()->onDelete('cascade');
+        $table->timestamps();
+    });
+}
 
     /**
      * Reverse the migrations.
