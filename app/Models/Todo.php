fb3a2384512866b93717b7fe2a4d640d7a8d70fc
John Carlo Yhapon <yhaponjc@gmail.com>
2024-12-12 21:33:20 +0800

    add title completed and user_id in the fillable

--- app/Models/Todo.php
+++ app/Models/Todo.php
@@ -3,8 +3,12 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 
 class Todo extends Model
 {
-    //
+    use HasFactory;
+
+    // Add 'title' to the $fillable array
+    protected $fillable = ['title', 'completed', 'user_id'];
 }
